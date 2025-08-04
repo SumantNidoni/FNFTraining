78fed23b4c14c79b7216a04ac292f7e033f50ec8
Sumant Nidoni <133426502+SumantNidoni@users.noreply.github.com>
2025-08-04 12:13:21 +0530

    Add files via upload

---
+++ Question2.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathons
+{
+    internal class Question2
+    {
+        static void Main(string[] args)
+        {
+            //------------------Second Program------------------
+            Console.WriteLine("Enter the number of books");
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> books = new List<string>();
+            Console.WriteLine("Enter the book entries in format: \"Title\" by Author");
+
+            for (int i = 0; i < n; i++)
+            {
+                books.Add(Console.ReadLine());
+            }
+
+            List<string> sortedTitles = GetSortedBookTitle(books);
+
+            Console.WriteLine("Sorted Book Titles");
+            foreach (var title in sortedTitles)
+            {
+                Console.WriteLine(title);
+            }
+        }
+
+
+        public static List<string> GetSortedBookTitle(List<string> books)
+        {
+
+            List<BookEntry> bookEntries = new List<BookEntry>();
+
+            foreach (string entry in books)
+            {
+                string[] parts = entry.Split(" by ");
+                string title = parts[0].Trim('\"').Trim();
+                string authorPart = parts[1].Trim();
+
+                string firstAuthor = authorPart.Split(',')[0].Trim();
+
+                bookEntries.Add(new BookEntry { Title = title, Author = firstAuthor });
+            }
+
+
+
+            for (int i = 0; i < bookEntries.Count - 1; i++)
+            {
+                for (int j = i + 1; j < bookEntries.Count; j++)
+                {
+                    BookEntry a = bookEntries[i];
+                    BookEntry b = bookEntries[j];
+
+                    bool swap = false;
+
+                    int authorCompare = string.Compare(a.Author, b.Author, StringComparison.OrdinalIgnoreCase);
+
+                    if (authorCompare > 0)
+                    {
+                        swap = true;
+                    }
+                    else if (authorCompare == 0)
+                    {
+                        int titleCompare = string.Compare(a.Title, b.Title, StringComparison.OrdinalIgnoreCase);
+                        if (titleCompare > 0)
+                        {
+                            swap = true;
+                        }
+                    }
+
+                    if (swap)
+                    {
+                        BookEntry temp = bookEntries[i];
+                        bookEntries[i] = bookEntries[j];
+                        bookEntries[j] = temp;
+                    }
+                }
+            }
+
+
+            List<string> sortedTitle = new List<string>();
+
+            foreach (var book in bookEntries)
+            {
+                sortedTitle.Add(book.Title);
+            }
+            return sortedTitle;
+        }
+    }
+    class BookEntry
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
+
+
---
+++ Questions1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathons
+{
+    internal class Questions1
+    {
+        static void Main(string[] args)
+        {
+            //------------------First Program------------------
+            Console.WriteLine("Enter the sentence");
+            List<string> res = WordFrequencyCounter(Console.ReadLine().ToLower());
+            foreach (string s in res)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+
+        public static List<string> WordFrequencyCounter(string input)
+        {
+            List<string> output = new List<string>();
+
+            bool hasLetter = false;
+            foreach (char c in input)
+            {
+                if (char.IsLetter(c))
+                {
+                    hasLetter = true;
+                    break;
+                }
+            }
+
+            if (!hasLetter)
+            {
+                output.Add("0");
+                return output;
+            }
+
+
+
+            char[] seperators = { ' ', '.', ',', '?', '!', ';', ':', '-', '\t', '\n' };
+
+            string[] words = input.Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+
+
+            Dictionary<string, int> frequency = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                if (frequency.ContainsKey(word))
+                {
+                    frequency[word]++;
+                }
+                else
+                {
+                    frequency[word] = 1;
+                }
+            }
+
+
+            List<KeyValuePair<string, int>> wordList = new List<KeyValuePair<string, int>>(frequency);
+
+
+            wordList.Sort((a, b) =>
+            {
+                int freqCompare = b.Value.CompareTo(a.Value);
+                return freqCompare != 0 ? freqCompare : string.Compare(b.Key, a.Key);
+            });
+
+            foreach (var pair in wordList)
+            {
+                output.Add($"{pair.Value} {pair.Key}");
+            }
+            return output;
+        }
+
+    }
+}
